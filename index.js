8c2ff97292c24bb85268e1513b541db799975e41
WinstonSmith13 <krouk.medy@gmail.com>
2023-03-08 17:45:09 +0100

    js: working on the fetch feature.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-function weatherSearch(name){
-    const key = 'abb89f2cb1c8aecb9127d7dddcd2e571';
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + name + ',CA&appid=' + key) 
-    .then((response)=> response.json())
-    .then((data)=> console.log(data));
-    
-}
+const container = document.querySelector('.container');
+const search = document.querySelector('.search_box button');
+const weatherBox = document.querySelector('.weather_box');
+const detailBox = document.querySelector('.detail_box');
+const error404 = document.querySelector('.not-found'); 
 
-weatherSearch('Paris')
+
+search.addEventListener('click', () => {
+    const key = 'abb89f2cb1c8aecb9127d7dddcd2e571';
+    
+    const city = document.querySelector('.search_box input').value;
+
+    if (city === '')
+    return;
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + ',CA&appid=' + key)
+    .then((response) => response.json())
+    .then(json => {
+        if (json.cod === '404'){
+            console.log('error')
+        }
+
+    })
+        
+
+
+});
